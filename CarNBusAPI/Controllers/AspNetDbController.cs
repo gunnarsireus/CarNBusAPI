583ad56fac5a08753d10992a73c210a94e948f63
gunnarsireus <gunnar.sireus@gmail.com>
2018-03-18 00:18:17 +0100

    ClearDatabase added

--- CarNBusAPI/Controllers/AspNetDbController.cs
+++ CarNBusAPI/Controllers/AspNetDbController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.Extensions.Configuration;
 using Shared.Utils;
+using Shared.Messages.Commands;
+using System;
+using NServiceBus;
 
 namespace CarNBusAPI.Controllers
 {
     [Route("api/[controller]")]
 	public class AspNetDbController : Controller
 	{
-		public AspNetDbController(IConfigurationRoot configuration)
+        readonly IEndpointInstance _endpointInstancePriority;
+        public AspNetDbController(IEndpointInstance endpointInstancePriority, IConfigurationRoot configuration)
 		{
 			Configuration = configuration;
-		}
+            _endpointInstancePriority = endpointInstancePriority;
+        }
 		IConfigurationRoot Configuration { get; set; }
 		// GET api/Car
 		[HttpGet]
@@ -23,5 +28,17 @@ namespace CarNBusAPI.Controllers
             return Helpers.GetAspNetDbConnection();
 
         }
-	}
+        [HttpPost("/api/aspnetdb/clear")]
+        [EnableCors("AllowAllOrigins")]
+        public void ClearDatabase()
+        {
+            var message = new ClearDatabase
+            {
+                DataId = Guid.NewGuid()
+            };
+
+            _endpointInstancePriority.Send(message).ConfigureAwait(false);
+        }
+
+    }
 }
