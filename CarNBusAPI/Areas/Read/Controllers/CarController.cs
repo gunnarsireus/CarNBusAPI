7e3877477006492039013d2bc764957786459093
gunnarsireus <gunnar.sireus@gmail.com>
2018-04-12 20:03:22 +0200

    msg queue algorithm perfected

--- CarNBusAPI/Areas/Read/Controllers/CarController.cs
+++ CarNBusAPI/Areas/Read/Controllers/CarController.cs
@@ -32,87 +32,8 @@ namespace CarNBusAPI.Read.Controllers
         // GET api/Car
         [HttpGet]
         [EnableCors("AllowAllOrigins")]
-        public IEnumerable<CarRead> GetCars()
+        public async Task<IEnumerable<CarRead>> GetCars()
         {
-            var list = new List<CarRead>();
-            var cars = _dataAccess.GetCars();
-            foreach (var car in cars)
-            {
-                if (car.Locked)
-                {
-                    if (new DateTime(car.LockedTimeStamp).AddMilliseconds(20000) < DateTime.Now)
-                    {  //Lock timeouted can be ignored and set to false
-                        var message = new UpdateCarLockedStatus
-                        {
-                            LockedStatus = false,
-                            CarId = car.CarId,
-                            CompanyId = car.CompanyId,
-                            UpdateCarLockedTimeStamp = DateTime.Now.Ticks
-                        };
-
-                        _endpointInstancePriority.Send(message).ConfigureAwait(false);
-                        car.Locked = false;
-                    }
-                }
-
-                list.Add(new CarRead(car.CarId)
-                {
-                    CompanyId = car.CompanyId,
-                    CreationTime = car.CreationTime,
-                    Locked = car.Locked,
-                    Online = car.Online,
-                    Speed = car.Speed,
-                    RegNr = car.RegNr,
-                    VIN = car.VIN
-                });
-            }
-            return list;
-        }
-        static string WriteOutMessage(CloudQueueMessage message)
-        {
-            var json = message.AsString;
-            var byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
-            if (json.StartsWith(json))
-            {
-                json = json.Remove(0, byteOrderMarkUtf8.Length);
-            }
-            dynamic parsedJson = JsonConvert.DeserializeObject(json);
-            var body = (string)parsedJson.Body;
-            byte[] data = Convert.FromBase64String(body);
-            return Encoding.UTF8.GetString(data);
-
-        }
-        static async Task<Dictionary<string, int>> GetCarsAndQueueLenght()
-        {
-            CloudStorageAccount storageAccount = QueueStorage.Common.CreateStorageAccountFromConnectionString(Helpers.GetStorageConnection());
-            CloudQueueClient cloudQueueClient = storageAccount.CreateCloudQueueClient();
-            var queueName = "carnbusapi-server";
-            CloudQueue queue = cloudQueueClient.GetQueueReference(queueName);
-            IEnumerable<CloudQueueMessage> peekedMessages = await queue.PeekMessagesAsync(32);
-            if (peekedMessages != null)
-            {
-                foreach (var msg in peekedMessages.ToList())
-                {
-                    //Console.WriteLine("WriteOutMessage(msg): " + WriteOutMessage(msg));
-                }      
-            }
-            return new Dictionary<string, int>
-            {
-                { "ABC123", 7 },
-                { "DEF456", 3 },
-                { "GHI789", 4 },
-                { "JKL012", 2 },
-                { "MNO345", 9 },
-                { "PQR678", 3 },
-                { "STU901", 3 }
-            };
-        }
-
-        [HttpGet("/api/read/carandqueuelength")]
-        [EnableCors("AllowAllOrigins")]
-        public async Task<IEnumerable<CarRead>> GetCarsAndQueLengthAsync()
-        {
-            Dictionary<string, int> carsAndQueueLength = await GetCarsAndQueueLenght();
             var list = new List<CarRead>();
             var cars = _dataAccess.GetCars();
             foreach (var car in cars)
@@ -142,8 +63,96 @@ namespace CarNBusAPI.Read.Controllers
                     Online = car.Online,
                     Speed = car.Speed,
                     RegNr = car.RegNr,
+                    VIN = car.VIN
+                });
+            }
+            return list;
+        }
+
+        static string GetCarIdFromMessage(CloudQueueMessage message)
+        {
+            var json = message.AsString;
+            var byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
+            if (json.StartsWith(json))
+            {
+                json = json.Remove(0, byteOrderMarkUtf8.Length);
+            }
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            var body = (string)parsedJson.Body;
+            byte[] data = Convert.FromBase64String(body);
+            string tmp = Encoding.UTF8.GetString(data);
+            return tmp.Substring(tmp.IndexOf("CarId") + 8, 36);
+        }
+
+        static async Task<Dictionary<string, int>> GetCarsAndQueueLenght()
+        {
+            Dictionary<string, int> guidQueueLength = new Dictionary<string, int>();
+            CloudStorageAccount storageAccount = QueueStorage.Common.CreateStorageAccountFromConnectionString(Helpers.GetStorageConnection());
+            CloudQueueClient cloudQueueClient = storageAccount.CreateCloudQueueClient();
+            var queueName = "carnbusapi-server";
+            CloudQueue queue = cloudQueueClient.GetQueueReference(queueName);
+            IEnumerable<CloudQueueMessage> peekedMessages = await queue.PeekMessagesAsync(32);
+
+            if (peekedMessages != null)
+            {
+                foreach (var msg in peekedMessages.ToList())
+                {
+                    var carId = GetCarIdFromMessage(msg);
+                    if (guidQueueLength.Any(a => a.Key == carId))
+                    {
+                        int oldValue = guidQueueLength.First(k => k.Key == carId).Value;
+                        guidQueueLength[carId] = oldValue++;
+                    }
+                    else
+                    {
+                        guidQueueLength.Add(carId, 1);
+                    }
+                }
+            }
+            return guidQueueLength;
+        }
+
+        [HttpGet("/api/read/carandqueuelength")]
+        [EnableCors("AllowAllOrigins")]
+        public async Task<IEnumerable<CarRead>> GetCarsAndQueLengthAsync()
+        {
+            Dictionary<string, int> carsAndQueueLength = await GetCarsAndQueueLenght();
+            var list = new List<CarRead>();
+            var cars = _dataAccess.GetCars();
+            foreach (var car in cars)
+            {
+                if (car.Locked)
+                {
+                    if (new DateTime(car.LockedTimeStamp).AddMilliseconds(20000) < DateTime.Now)
+                    {  //Lock timeouted can be ignored and set to false
+                        var message = new UpdateCarLockedStatus
+                        {
+                            LockedStatus = false,
+                            CarId = car.CarId,
+                            CompanyId = car.CompanyId,
+                            UpdateCarLockedTimeStamp = DateTime.Now.Ticks
+                        };
+
+                        await _endpointInstancePriority.Send(message).ConfigureAwait(false);
+                        car.Locked = false;
+                    }
+                }
+                int tmpLength = 0;
+                if (carsAndQueueLength.Any(k => k.Key == car.CarId.ToString()))
+                {
+                    tmpLength = carsAndQueueLength.FirstOrDefault(k => k.Key == car.CarId.ToString()).Value;
+                };
+
+                list.Add(new CarRead(car.CarId)
+                {
+                    CompanyId = car.CompanyId,
+                    CreationTime = car.CreationTime,
+                    Locked = car.Locked,
+                    Online = car.Online,
+                    Speed = car.Speed,
+                    RegNr = car.RegNr,
                     VIN = car.VIN,
-                    QueueLength = carsAndQueueLength.FirstOrDefault(k => k.Key == car.RegNr).Value
+                    QueueLength = tmpLength
                 });
             }
             return list;
@@ -151,7 +160,7 @@ namespace CarNBusAPI.Read.Controllers
         // GET api/Car/5
         [HttpGet("{id}")]
         [EnableCors("AllowAllOrigins")]
-        public CarRead GetCar(string id)
+        public async Task<CarRead> GetCar(string id)
         {
             var car = _dataAccess.GetCar(new Guid(id));
             if (car.Locked)
@@ -166,7 +175,7 @@ namespace CarNBusAPI.Read.Controllers
                         UpdateCarLockedTimeStamp = DateTime.Now.Ticks
                     };
 
-                    _endpointInstancePriority.Send(message).ConfigureAwait(false);
+                    await _endpointInstancePriority.Send(message).ConfigureAwait(false);
                     car.Locked = false;
                 }
             }
