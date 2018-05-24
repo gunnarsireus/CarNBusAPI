981e17285090ba27994947dddf12b2be90898e1f
Gunnar Siréus <gunnar.sireus@gmail.com>
2018-05-24 19:32:32 +0200

    Added EndpointPriority

--- CarNBusAPI/Startup.cs
+++ CarNBusAPI/Startup.cs
@@ -9,6 +9,8 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Shared.Utils;
 using Shared.Messages.Events;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace CarNBusAPI
 {
@@ -24,27 +26,54 @@ namespace CarNBusAPI
         }
 
         IEndpointInstance EndpointInstance { get; set; }
+        IEndpointInstance EndpointInstancePriority { get; set; }
         Autofac.IContainer Container { get; set; }
         IConfigurationRoot ConfigurationRoot { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var endpointConfiguration = Helpers.CreateEndpoint(Helpers.GetDbLocation(ConfigurationRoot["AppSettings:DbLocation"]), "carnbusapi-client");
-            endpointConfiguration.UsePersistence<AzureStoragePersistence>()
-                                 .ConnectionString(Helpers.GetStorageConnection());
+            endpointConfiguration.UsePersistence<AzureStoragePersistence, StorageType.Subscriptions>()
+                           .ConnectionString(Helpers.GetStorageConnection());
+
+            endpointConfiguration.UsePersistence<AzureStoragePersistence, StorageType.Timeouts>()
+           .ConnectionString(Helpers.GetStorageConnection())
+           .CreateSchema(true)
+           .TimeoutManagerDataTableName("TimeoutManager")
+           .TimeoutDataTableName("TimeoutData")
+           .CatchUpInterval(3600)
+           .PartitionKeyScope("2018052400");
 
             var transport = endpointConfiguration.UseTransport<AzureStorageQueueTransport>()
                                         .ConnectionString(Helpers.GetStorageConnection());
 
-            transport.Routing().RouteToEndpoint(assembly: typeof(CreateCar).Assembly,@namespace: "Shared.Messages.Commands", destination: "carnbusapi-server");
-            transport.Routing().RouteToEndpoint(assembly: typeof(UpdateCarLockedStatus).Assembly,@namespace: "Shared.Messages.Events", destination: "carnbusapi-serverpriority");
+            transport.Routing().RouteToEndpoint(assembly: typeof(CreateCar).Assembly, @namespace: "Shared.Messages.Commands", destination: "carnbusapi-server");
             EndpointInstance = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            var endpointConfigurationPriority = Helpers.CreateEndpoint(Helpers.GetDbLocation(ConfigurationRoot["AppSettings:DbLocation"]), "carnbusapi-clientPriority");
+            endpointConfigurationPriority.UsePersistence<AzureStoragePersistence, StorageType.Subscriptions>()
+                           .ConnectionString(Helpers.GetStorageConnection());
+
+            endpointConfigurationPriority.UsePersistence<AzureStoragePersistence, StorageType.Timeouts>()
+           .ConnectionString(Helpers.GetStorageConnection())
+           .CreateSchema(true)
+           .TimeoutManagerDataTableName("TimeoutManagerPriority")
+           .TimeoutDataTableName("TimeoutDataPriority")
+           .CatchUpInterval(3600)
+           .PartitionKeyScope("2018052400");
+
+            var transportPriority = endpointConfigurationPriority.UseTransport<AzureStorageQueueTransport>()
+                                        .ConnectionString(Helpers.GetStorageConnection());
+
+            transportPriority.Routing().RouteToEndpoint(assembly: typeof(UpdateCarLockedStatus).Assembly,@namespace: "Shared.Messages.Events", destination: "carnbusapi-serverpriority");
+            EndpointInstancePriority = Endpoint.Start(endpointConfigurationPriority).GetAwaiter().GetResult();
 
             var containerBuilder = new ContainerBuilder();
             containerBuilder.Populate(services);
 
             Container = containerBuilder.Build();
             services.AddSingleton(EndpointInstance);
+            services.AddSingleton(EndpointInstancePriority);
             services.AddSingleton(ConfigurationRoot);
             services.AddMvc();
 
@@ -64,8 +93,12 @@ namespace CarNBusAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
+            Console.WriteLine("Configure...");
+            loggerFactory.AddConsole(ConfigurationRoot.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            appLifetime.ApplicationStopped.Register(() => Container.Dispose());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
