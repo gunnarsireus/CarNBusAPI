ecd4ef16193d4bf56829558daff1f92bb12b3c46
Gunnar Siréus <gunnar.sireus@gmail.com>
2018-07-11 19:34:18 +0200

    Refactorng, addes GetCompanyById

--- GetCompanies/GetCompanies.cs
+++ GetCompanies/GetCompanies.cs
@@ -1,21 +1,19 @@
-
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
 using Shared.DAL;
 using System.Collections.Generic;
 using Shared.Models.Read;
 
 namespace Company
 {
-    public static class Company
+    public static class GetCompanies
     {
         static readonly DataAccessRead _dataAccessRead = new DataAccessRead();
-        [FunctionName("Company")]
+        // Azure Functions HTTP Trigger
+        // Endpoint URL: /api/products/{id}
+        [FunctionName("GetCompanies")]
         public static IEnumerable<CompanyRead> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed GetCompanies.");
---
+++ GetCompanyById/GetCompanyByID.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Shared.DAL;
+using Shared.Models.Read;
+using System;
+
+namespace GetCompanyById
+{
+    public static class GetCompanyByID
+    {
+        // Azure Functions HTTP Trigger
+        // Endpoint URL: /api/products/{id}
+        static readonly DataAccessRead _dataAccessRead = new DataAccessRead();
+        [FunctionName("GetCompanyById")]
+        public static CompanyRead Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, Guid id, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed GetCompanyById.");
+            return _dataAccessRead.GetCompany(id);
+        }
+    }
+}
