1d5e7ba4c8a431658bb0cfcb0a5bb4222b1e1897
Gunnar Siréus <gunnar.sireus@gmail.com>
2018-07-11 19:47:03 +0200

    Refactoring

--- GetCompanies/GetCompanies.cs
+++ GetCompanies/GetCompanies.cs
@@ -3,20 +3,19 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Shared.DAL;
-using System.Collections.Generic;
 using Shared.Models.Read;
+using System.Collections.Generic;
 
-namespace Company
+namespace GetCompanies
 {
     public static class GetCompanies
     {
         static readonly DataAccessRead _dataAccessRead = new DataAccessRead();
-        // Azure Functions HTTP Trigger
-        // Endpoint URL: /api/products/{id}
+
         [FunctionName("GetCompanies")]
         public static IEnumerable<CompanyRead> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed GetCompanies.");
+            log.Info("C# HTTP trigger function processed GetCompanyById.");
             return _dataAccessRead.GetCompanies();
         }
     }
